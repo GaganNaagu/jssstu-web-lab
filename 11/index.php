47cd0424986343281eae628dc862d4f8737df266
GaganNaagu <GaganSG1908@gmail.com>
2026-06-25 11:57:15 +0530

    Update program 11 to include clear file functionality

--- 11/index.php
+++ 11/index.php
@@ -9,19 +9,25 @@
         <label>Information:</label><br>
         <textarea name="info" rows="4" cols="30" required></textarea><br><br>
         <button type="submit" name="submit">Submit</button>
-        <button type="reset">Reset</button>
+        <button type="submit" name="reset" formnovalidate>Clear File</button>
     </form>
 
     <?php
-    if (isset($_POST['submit'])) {
-        $info = $_POST['info'];
-        $file = 'data.txt';
+    $file = 'data.txt';
 
-        // Append to file
+    if (isset($_POST['reset'])) {
+        file_put_contents($file, ""); // Clears the file
+        echo "<h3>File cleared successfully!</h3>";
+    } elseif (isset($_POST['submit'])) {
+        $info = $_POST['info'];
+
         file_put_contents($file, $info . PHP_EOL, FILE_APPEND);
 
+        $storedData = file_get_contents($file);
+
         echo "<h3>Information stored successfully!</h3>";
-        echo "<b>You entered:</b> " . htmlspecialchars($info);
+        echo "<b>Contents of data.txt:</b><br>";
+        echo $storedData;
     }
     ?>
 </body>
